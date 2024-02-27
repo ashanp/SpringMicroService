37dd38077831f8ae1540192dcb7a839dbc0f6c9b
ashanperera <ashanp@gmail.com>
2024-02-27 17:12:26 +0400

    added service-configuration module

---
+++ service-configuration/src/main/java/com/perera/Configuration.java
@@ -0,0 +1,13 @@
+package com.perera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Configuration {
+    public static void main(String[] args){
+        SpringApplication.run(Configuration.class, args);
+    }
+}
